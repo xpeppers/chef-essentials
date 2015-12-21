ee1a830cc04ee0d57553a6438d37ee48a51dd5fa
Filippo Liverani <fili_live@yahoo.it>
2015-12-21 11:17:12 +0100

    Added force option to vagrant box import

--- start.sh
+++ start.sh
@@ -5,5 +5,5 @@ VIRTUALBOX_HOME=/c/Program\ Files/Oracle/VirtualBox
 
 export PATH="${PATH}:${VAGRANT_HOME}/bin:${VIRTUALBOX_HOME}"
 
-vagrant box add --name xpeppers/chef-essentials chef-essentials.box
+vagrant box add --force --name xpeppers/chef-essentials chef-essentials.box
 vagrant up
